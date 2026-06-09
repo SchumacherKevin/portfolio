c79abafcf0aafd1ceaabc9cfa63113053b2883f0
Kevin Schumacher <KevinSchumacher@t-online.de>
2026-06-09 23:32:22 +0200

    added routes and folder structer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home').then(m => m.HomeComponent)
+  },
+  {
+    path: 'legal-notice',
+    loadComponent: () => import('./pages/legal-notice/legal-notice').then(m => m.LegalNoticeComponent)
+  },
+  {
+    path: 'docs',
+    loadComponent: () => import('./pages/docs/docs').then(m => m.DocsComponent)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
