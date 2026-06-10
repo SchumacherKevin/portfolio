d33bd617afd76ab669b312b31e9cfa543926f4ee
Kevin Schumacher <KevinSchumacher@t-online.de>
2026-06-10 16:12:02 +0200

    set global variables in scss, deleted docs, added font

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,11 +9,7 @@ export const routes: Routes = [
     path: 'legal-notice',
     loadComponent: () => import('./pages/legal-notice/legal-notice').then(m => m.LegalNoticeComponent)
   },
-  {
-    path: 'docs',
-    loadComponent: () => import('./pages/docs/docs').then(m => m.DocsComponent)
-  },
-  {
+{
     path: '**',
     redirectTo: ''
   }
