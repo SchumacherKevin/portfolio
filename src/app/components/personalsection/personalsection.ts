eada91b969a3cbd3bc38fd73910ccc7a0416c737
Kevin Schumacher <KevinSchumacher@t-online.de>
2026-06-15 11:01:32 +0200

    feat:empty components added

---
+++ src/app/components/personalsection/personalsection.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personalsection',
+  imports: [],
+  templateUrl: './personalsection.html',
+  styleUrl: './personalsection.scss',
+})
+export class Personalsection {}
