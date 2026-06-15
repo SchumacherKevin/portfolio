f4d9e0c6b3ca97f16c6e2c02175e9c96b9f68527
Kevin Schumacher <KevinSchumacher@t-online.de>
2026-06-15 19:40:51 +0200

    feat : hersection merched from both pc´s, style adjusted

--- src/app/components/herosection/herosection.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-herosection',
-  imports: [],
-  templateUrl: './herosection.html',
-  styleUrl: './herosection.scss',
-})
-export class Herosection {}
