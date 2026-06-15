3e9a39e0e8ff8e249e2d5fd988d0c3171f2513b7
Kevin Schumacher <KevinSchumacher@t-online.de>
2026-06-15 23:27:27 +0200

    skill section finished and shown, styles adjusted

--- src/app/components/skillsection/skillsection.ts
+++ src/app/components/skillsection/skillsection.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
 
+interface Skill {
+  label: string;
+  icon: string;
+  challenge?: boolean;
+}
+
 @Component({
   selector: 'app-skillsection',
-  imports: [],
+  standalone: true,
   templateUrl: './skillsection.html',
   styleUrl: './skillsection.scss',
 })
-export class Skillsection {}
+export class Skillsection {
+  skills: Skill[] = [
+    { label: 'Angular', icon: 'assets/angular-icon-v2.svg' },
+    { label: 'TypeScript', icon: 'assets/typescript-icon.svg' },
+    { label: 'JavaScript', icon: 'assets/javascript-icon.svg' },
+    { label: 'HTML', icon: 'assets/html5-icon.svg' },
+    { label: 'CSS', icon: 'assets/css3-icon.svg' },
+    { label: 'REST-API', icon: 'assets/restapi-icon.svg' },
+    { label: 'Supabase', icon: 'assets/supabase-icon.svg' },
+    { label: 'Git', icon: 'assets/git-icon.svg' },
+    { label: 'Scrum', icon: 'assets/scrum-icon.svg' },
+    { label: 'Material design', icon: 'assets/materialdesign-icon.svg' },
+    { label: 'Challenge me', icon: 'assets/question-box-icon.svg', challenge: true },
+  ];
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero';
 import { Personalsection } from '../../components/personalsection/personalsection';
+import { Skillsection } from '../../components/skillsection/skillsection';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrl: './home.scss',
   standalone: true,
-  imports: [HeroComponent, Personalsection]
+  imports: [HeroComponent, Personalsection, Skillsection]
 })
 export class HomeComponent { }
